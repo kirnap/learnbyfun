f44cf5717563e13012b44ffab888650fba76e952
OmerKirnap <okirnap@ku.edu.tr>
2017-11-09 23:07:48 +0300

    WIP modify dictionary

--- PYTHNON/eng_dict.py
+++ PYTHNON/eng_dict.py
@@ -24,27 +24,56 @@ from bs4 import BeautifulSoup
 url2 = "http://googledictionary.freecollocation.com/meaning?word="
 #query2 = raw_input("What do you want to look for meaning? >> ")
 
+def retrieve_sentence(ugly_text):
+    """
+    Retrieves a sentence from a given list
+    """
+    alist = ugly_text.text.split()
+    sents = []
+    sent = ""
+    for item in alist:
+        if item != "-" and item != alist[-1]:
+            sent += item + " "
+        #elif item == alist[-1]:
+        #    sents.append(sent)
+        else:
+            sents.append(sent)
+            sent = ""
+    return sents
+
+
 
 def get_mean(query):
     rep = urllib2.urlopen((url2+query)).read()
-    soup = BeautifulSoup(rep)
-    meaningful_text = soup.find('div', {'class' : 'std'}).text.split()
-    counter = 0
-    for item in meaningful_text:
-        if item == '-' and counter == 0:
-            print('\n  1.') , 
-            counter += 2
-        else:
-            if counter == 0:
-                print(item) ,
-            else:
-                if item == '-':
-                    print('\n  '+str(counter)+'.') ,
-                elif item[0].isupper():
-                    counter -= 1
-                    print('\n' + item) ,
-                else:
-                    print(item) ,
+    soup = BeautifulSoup(rep, "html.parser")
+    #meaningful_text = soup.find('div', {'class' : 'std'}).text.split()
+    #color:#767676;list-style:none
+    all_examples = soup.find_all(attrs={"style" : "color:#767676;list-style:none"})
+    alltxt = soup.find_all(attrs={"style" : "list-style:decimal"})
+    for i in range(len(alltxt) - 1):
+        s = list(retrieve_sentence(alltxt[i]) - retrieve_sentence(alltxt[i+1]))
+        for item in s:
+            print item
+        
+        
+
+    # counter = 0
+    # for item in meaningful_text:
+    #     if item == '-' and counter == 0:
+    #         print('\n  1.') , 
+    #         counter += 2
+    #     else:
+    #         if counter == 0:
+    #             print(item) ,
+    #         else:
+    #             if item == '-':
+    #                 print('\n  '+str(counter)+'.') ,
+    #             elif item[0].isupper() or item[0] == '(':
+    #                 #print("Debug: ", item) ,
+    #                 counter -= 1
+    #                 print('\n' + item) ,
+    #             else:
+    #                 print(item) ,
 
 
 if __name__ == '__main__':

355bc9bcc7762ff8fdc1712265abbb4f47c5b2a6
OmerKirnap <okirnap@ku.edu.tr>
2017-11-09 14:04:33 +0300

    add personal dictionary fun

---
+++ PYTHNON/eng_dict.py
@@ -0,0 +1,48 @@
+# command line English Dictionary, written by Omer Kirnap
+# Usage python eng_dict.py
+import urllib2
+from bs4 import BeautifulSoup
+#import urllib
+
+#url = "https://www.google.com/search?"
+
+#query = raw_input("What do you want to look for meaning? >> ")
+
+#query = urllib.urlencode( {'q' : query } )
+
+# Here urlopen returns 403 forbidden 
+#response = urllib2.urlopen (url + query ).read()
+
+# Instead of that if I use that, I couldn't be able to succesfully retrieve the text that I want,
+# I checked with one of the online html editor to retrieve html document back and couldn't see the same output page
+#response2 = requests.get(url + query)
+#soup = BeautifulSoup(response2.content)
+
+
+##### Solution from another page
+url2 = "http://googledictionary.freecollocation.com/meaning?word="
+query2 = raw_input("What do you want to look for meaning? >> ")
+rep = urllib2.urlopen((url2+query2)).read()
+soup = BeautifulSoup(rep)
+omer = soup.find('div', {'class' : 'std'}).text.split()
+
+
+
+counter = 0
+for item in omer:
+    if item == '-' and counter == 0:
+        print('\n  1.') , 
+        counter += 2
+    else:
+        if counter == 0:
+            print(item) ,
+        else:
+            if item == '-':
+                print('\n  '+str(counter)+'.') ,
+            elif item[0].isupper():
+                counter -= 1
+                print('\n' + item) ,
+            else:
+                print(item) ,
+
+

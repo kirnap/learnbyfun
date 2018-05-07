d14a88bf3136a2429cae7391007462d5bb196d80
Omer Kirnap <okirnap@ku.edu.tr>
2018-05-08 01:11:17 +0300

    add prediction

---
+++ JULIA/polyvore/predict.jl
@@ -0,0 +1,17 @@
+include("train.jl")
+
+function predme()
+    resmodel = create_resmodel()
+    seqid = 31011385
+    imds = [5,2,4,1]
+    sort!(imds)
+    bundle = JLD.load("600_all_best_model.jld")
+    model, ss, se = bundle["model"], bundle["ss"], bundle["se"]
+    inputs = propfmod(seqid, imds, resmodel)
+
+    inputs = map(x->reshape(x, length(x), 1), inputs)
+    f = (gpu()>= 0 ? KnetArray : Array)
+    in2lstm = map(x->wembed(model)*f(x), inputs)
+    # give half of them to forward and the other to backward
+    
+end

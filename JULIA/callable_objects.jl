d56ae39f04edec88f8047e3af7d9c5e87730f5ea
OmerKirnap <okirnap@ku.edu.tr>
2018-09-24 11:24:25 +0300

    add callable objects

---
+++ JULIA/callable_objects.jl
@@ -0,0 +1,25 @@
+# This script aims to discover 'callable objects in julia'
+
+# holds the coefficients of a polynomial
+# e.g. 100x^2 + 10x + 1 -> Polynomial([1,10,100])
+struct Polynomial{T}
+    coeffs::Vector{T}
+end
+
+
+# Used to calculate polynomial's value
+function (p::Polynomial)(x) 
+    v = p.coeffs[end]
+    for k in 1:length(v)
+        v = v*x + p.coeffs[k]
+    end
+    return v[end]
+end
+
+(p::Polynomial)() = p(5) # to define default caller
+
+#=
+julia> p = Polynomial([1,10,100])
+Polynomial{Int64}([1, 10, 100])
+
+=#
